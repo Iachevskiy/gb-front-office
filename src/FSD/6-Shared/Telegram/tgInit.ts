b333bf26074a1ca03b6aeb15b28f9cb3725d8e30
vladimiriachevskii <ggg@jj.yy>
2025-02-20 10:05:55 +0500

    check-viewport

--- src/FSD/6-Shared/Telegram/tgInit.ts
+++ src/FSD/6-Shared/Telegram/tgInit.ts
@@ -1,14 +1,24 @@
-import { onMounted } from "vue";
 
 import { init, postEvent } from "@telegram-apps/sdk-vue";
 
-export default () => {
-  onMounted(() => {
+export const tgInit = () => {
+  try {
     init();
 
     // postEvent("web_app_setup_swipe_behavior", { allow_vertical_swipe: true });
     postEvent("web_app_toggle_orientation_lock", { locked: true });
     postEvent("web_app_expand");
+    postEvent("web_app_set_background_color", { color: "#467A2A8E" });
+    postEvent("web_app_set_bottom_bar_color", { color: "#83D5BB8E" });
 
-  });
+    // web_app_set_background_color
+
+    // const removeListener = on("viewport_changed", (payload) => {
+    //   console.log("Viewport changed:", payload);
+    // });
+  }
+  catch (error) {
+    console.warn(error);
+  }
+
 };
